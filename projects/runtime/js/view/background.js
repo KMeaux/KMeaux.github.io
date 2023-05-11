9559891d7cc2ecf1c01dd0f7fd340b42af371265
KMeaux <kem0199801@lpssonline.com>
2023-05-11 14:02:29 +0000

    started runtime project

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -26,7 +26,7 @@ var background = function (window) {
         var background;
         
         // ANIMATION VARIABLES HERE:
-        
+        var tree;
      
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
@@ -36,17 +36,32 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'lavender');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
+            var moon = draw.bitmap("img/moon.png");
+            moon.x = 1750;
+            moon.y = 30;
+            moon.scaleX = 0.2;
+            moon.scaleY = 0.2;
+            background.addChild(moon);
             
+            for (var i = 100; i < 1000; i++) {
+                var circle = draw.circle(2.5, "white", "LightGray", 1);
+                circle.x = canvasWidth * Math.random();
+                circle.y = groundY * Math.random();
+                background.addChild(circle);
+            }
             
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
             
             // TODO 4: Part 1 - Add a tree
-            
+            tree = draw.bitmap("img/tree.png");
+            tree.x = 400;
+            tree.y = groundY - 230;
+            background.addChild(tree);
             
         } // end of render function - DO NOT DELETE
         
